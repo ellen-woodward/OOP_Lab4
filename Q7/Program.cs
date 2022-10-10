224528269bd7aef7bf798f5e273018c1676dd2a5
Ellen Woodward <S00222321@atu.ie>
2022-10-10 12:43:29 +0100

    Q7 - Schools

---
+++ Q7/Program.cs
@@ -0,0 +1,26 @@
+﻿using Q7;
+
+School school1 = new School();
+AddInfo(school1);
+
+School school2 = new School();
+AddInfo(school2);
+
+School school3 = new School();
+AddInfo(school3);
+
+School school4 = new School();
+AddInfo(school4);
+
+School school5 = new School();
+AddInfo(school5);
+
+
+static void AddInfo(School school)
+{
+    Console.Write("\nEnter school name: ");
+    school.Name = Console.ReadLine();
+
+    Console.Write("\nEnter number of students: ");
+    school.NumberofStudents = int.Parse(Console.ReadLine());
+}
---
+++ Q7/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7
+{
+    internal class School
+    {
+        public string Name { get; set; }
+        public int NumberofStudents { get; set; }
+
+        public School()
+        {
+
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"{Name} School has {NumberofStudents} students");
+        }
+    }
+}
