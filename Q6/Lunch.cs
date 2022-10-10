29180f096c87c5e809d9a5fc1548774f340a9c9f
Ellen Woodward <S00222321@atu.ie>
2022-10-10 12:16:30 +0100

    Q6 - using params

---
+++ Q6/Lunch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q6
+{
+    internal class Lunch
+    {
+        public string Entree { get; set; }
+        public string Side { get; set; }
+        public string Drink { get; set; }
+
+        public Lunch(string entree, string side, string drink)
+        {
+            Entree = entree;
+            Side = side;
+            Drink = drink;
+        }
+    }
+}
---
+++ Q6/Program.cs
@@ -0,0 +1,23 @@
+﻿using Q6;
+
+Lunch lunch1 = new Lunch("beans", "cookie", "coffee");
+Lunch lunch2 = new Lunch("pizza", "fries", "milkshake");
+Lunch lunch3 = new Lunch("toastie", "salad", "water");
+Lunch lunch4 = new Lunch("hotdog", "fruit", "milk");
+Lunch lunch5 = new Lunch("soup", "bread", "soda");
+
+Display(lunch1, lunch2, lunch3);
+Console.WriteLine();
+Display(lunch1, lunch2, lunch3, lunch4);
+Console.WriteLine();
+Display(lunch1, lunch2, lunch3, lunch4, lunch5);
+
+static void Display(params Lunch[]lunches)
+{
+    Console.WriteLine("Entree\tSide\tDrink");
+
+    foreach (Lunch lunch in lunches)
+    {
+        Console.WriteLine($"{lunch.Entree}\t{lunch.Side}\t{lunch.Drink}");
+    }
+}
